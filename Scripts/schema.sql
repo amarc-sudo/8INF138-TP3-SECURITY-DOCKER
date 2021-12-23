b2f339c1519bfad9e4ff9bfcebfcd55566ccb4e5
Aurelien Marc <aurelien_marc@icloud.com>
2021-12-23 16:12:57 -0500

    FINISH-FIX : TP3 RAPPORT finsih and fix Docker

--- Scripts/schema.sql
+++ Scripts/schema.sql
@@ -53,3 +53,11 @@ create table tp3.token
     constraint token_user_idUser_fk
         foreign key (user) references tp3.user (idUser)
 );
+
+INSERT INTO tp3.role (idRole, tagRole, labelRole) VALUES (1, 'admin', 'admin');
+INSERT INTO tp3.role (idRole, tagRole, labelRole) VALUES (2, 'residentiel', 'residentiel');
+INSERT INTO tp3.role (idRole, tagRole, labelRole) VALUES (3, 'affaire', 'affaire');
+
+INSERT INTO tp3.user (idUser, login, password, salt, role, tentative) VALUES (1, 'admin', 'ZrXFHZD4wugvz3CrQwp0l5PqZWw0g+a5i3X69aTRku28ElianFKj0SV3zUcyUjlpj/uCLGRVJHD4G3dc0ailTA==', '5moLABpkJ+KeEpPtLngx2g==', 1, 0);
+INSERT INTO tp3.user (idUser, login, password, salt, role, tentative) VALUES (2, 'utilisateur1', 'ZrXFHZD4wugvz3CrQwp0l5PqZWw0g+a5i3X69aTRku28ElianFKj0SV3zUcyUjlpj/uCLGRVJHD4G3dc0ailTA==', '5moLABpkJ+KeEpPtLngx2g==', 2, 0);
+INSERT INTO tp3.user (idUser, login, password, salt, role, tentative) VALUES (3, 'utilisateur2', 'ZrXFHZD4wugvz3CrQwp0l5PqZWw0g+a5i3X69aTRku28ElianFKj0SV3zUcyUjlpj/uCLGRVJHD4G3dc0ailTA==', '5moLABpkJ+KeEpPtLngx2g==', 3, 0);
